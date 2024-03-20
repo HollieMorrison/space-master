c8022002b501d4d38e76c30275329fcfbf4399c4
HollieMorrison <104519409+HollieMorrison@users.noreply.github.com>
2024-03-20 22:42:31 +0000

    Fix: score counter works

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,45 +6,69 @@ const alienQuestion = document.getElementById('aliens_question');
 const spaceQuestion = document.getElementById('space_question');
 const butterflyQuestion = document.getElementById('butterfly_question');
 const questions = [alienQuestion, spaceQuestion, butterflyQuestion];
+
  
 
   
 let rightAnswers = 0;
 let totalQuestions = questions.length;
+let totalPoints = 0;
+
 
 document.getElementById('total_questions').textContent = totalQuestions;
 document.getElementById('right_answers').textContent = rightAnswers;
+document.getElementById('total_points').textContent = totalPoints;
+
+document.getElementById('quiznos').addEventListener('submit', function(e) {
+    e.preventDefault();
+    document.getElementById('right_answers').textContent = rightAnswers;
+    document.getElementById('total_points').textContent = totalPoints;
+});
 
 function checkAnswer(e) { 
-   
-    const answers = document.getElementById('answers');
     e.preventDefault();
     
-    
-    document.getElementById('right_answers').textContent = rightAnswers;
-    
+  
+   
     
     switch(alienQuestion.value) {
-    case 'millions':
+    case '1':
     rightAnswers+= 1;
+    totalPoints+= 100;
     break;
     default:
        console.log('wrong');
     }
+
     switch(spaceQuestion.value) {
-    case 'She doesnt know':
+    case '2':
     rightAnswers+= 1;
+    totalPoints+= 100;
     break;
     default:
        console.log('wrong');
     }
 
     switch(butterflyQuestion.value) {
-    case 'millions':
+    case '3':
     rightAnswers+= 1;
+    totalPoints+= 100;
     break;
     default:
        console.log('wrong');
     }
+
+    if (totalPoints >= 300) {
+    quiz.style.display = 'none';
+    
+    } 
+    if (totalPoints >= 300) {
+    winner.style.display = 'block';
+    
+    } 
   }
 }
+
+
+
+
