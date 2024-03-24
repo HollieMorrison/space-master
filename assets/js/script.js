82e041e1c2ebd6d09d3377e89178fd89392449cb
HollieMorrison <104519409+HollieMorrison@users.noreply.github.com>
2024-03-24 19:42:55 +0000

    Update: question layout changed to multiple choice radio buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,76 @@
 
 
 if (document.getElementById('quiznos')) {
-const quiz = document.getElementById('quiznos');
+var quiz = document.getElementById('quiznos');
 const alienQuestion = document.getElementById('aliens_question');
 const spaceQuestion = document.getElementById('space_question');
 const butterflyQuestion = document.getElementById('butterfly_question');
-const questions = [alienQuestion, spaceQuestion, butterflyQuestion];
+var questions = [
+   {
+      id: "moonQuestion",
+      title: "How far is the Moon?",
+      A: "it lives next door",
+      B: "no idea",
+      C: "i am sure it is pretty far",
+      D: "some km",
+      correctAnswer: "C"
+   },
+     {
+      id: "planetQuestion",
+      title: "What planet is closest to the sun?",
+      A: "it lives next door",
+      B: "no idea",
+      C: "i am sure it is pretty far",
+      D: "some km",
+      correctAnswer: "C"
+   },
+     {
+      id: "saturnQuestion",
+      title: "How many rings does Saturn have?",
+      A: "it lives next door",
+      B: "no idea",
+      C: "i am sure it is pretty far",
+      D: "Mercury",
+      correctAnswer: "D"
+   },
+] ;
 
- 
+
+var quizContainer = document.getElementById('quiznos');
+
+questions.forEach(function(question) {
+   var div = document.createElement('div.question');
+   div.innerHTML = `
+   <h2>${question.title}</h2>
+   <label for="${question.A}" class="form_elem">
+   <input type="radio" name="${question.id}" id="${question.id}-A">
+   ${question.A}
+    </label>
+       <label for="${question.B}" class="form_elem" >
+       <input type="radio" name="${question.id}" id="${question.id}-id="${question.id}-B">
+       ${question.B}
+    </label>
+       <label for="${question.C}" class="form_elem">
+       <input type="radio" name="${question.id}" id="${question.id}-C">
+       ${question.C}
+    </label>
+       <label for="${question.D}" class="form_elem">
+       <input type="radio" name="${question.id}" id="${question.id}-D">
+       ${question.D}
+    </label>
+
+
+   `;
+   quizContainer.appendChild(div);
+});
+
 
   
 let rightAnswers = 0;
 let totalQuestions = questions.length;
 let totalPoints = 0;
+
+var selectedAnswer = "";
 
 
 document.getElementById('total_questions').textContent = totalQuestions;
@@ -25,39 +83,27 @@ document.getElementById('quiznos').addEventListener('submit', function(e) {
     document.getElementById('total_points').textContent = totalPoints;
 });
 
+
+quiz.addEventListener('submit', function(e) {
+   e.preventDefault();
+   var selectedAnswer = document.querySelector('input[type="radio"]:checked').value;
+    console.log(selectedAnswer);
+
+});
+
+
 function checkAnswer(e) { 
     e.preventDefault();
+
+    if (questions.correctAnswer === selectedAnswer) {
+         rightAnswers++;
+         totalPoints += 100;
+      console.log('right answer' + questions.correctAnswer);
+    } else {
+      console.log('wrong answer' + questions.correctAnswer);
+    }
     
-  
-   
     
-    switch(alienQuestion.value) {
-    case '1':
-    rightAnswers+= 1;
-    totalPoints+= 100;
-    break;
-    default:
-       console.log('wrong');
-    }
-
-    switch(spaceQuestion.value) {
-    case '2':
-    rightAnswers+= 1;
-    totalPoints+= 100;
-    break;
-    default:
-       console.log('wrong');
-    }
-
-    switch(butterflyQuestion.value) {
-    case '3':
-    rightAnswers+= 1;
-    totalPoints+= 100;
-    break;
-    default:
-       console.log('wrong');
-    }
-
     if (totalPoints >= 300) {
     quiz.style.display = 'none';
     
@@ -72,3 +118,4 @@ function checkAnswer(e) {
 
 
 
+console.log(selectedAnswer)
