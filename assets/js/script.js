0cc09f58afaf5ad21e744c07aee0678ebe650255
HollieMorrison <104519409+HollieMorrison@users.noreply.github.com>
2024-03-26 21:07:35 +0000

    Update: Questions added with new id's

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// check if an element with the id 'quiznos' exists within the document
+// check if an element with the id 'quiznos' exists within the document.
 if (document.getElementById('quiznos')) {
 
 // get the element with the id 'quiznos'
@@ -33,40 +33,47 @@ var questions = [
       D: "Mercury",
       correctAnswer: "C"
    },
+    {
+      id: "saturnQuestionsss",
+      title: "How many rings does Saturn have?",
+      A: "it lives next door",
+      B: "no idea",
+      C: "i am sure it is pretty far",
+      D: "Mercury",
+      correctAnswer: "C"
+   },
+    {
+      id: "saturnQuestionsssss",
+      title: "How many rings does Saturn have?",
+      A: "it lives next door",
+      B: "no idea",
+      C: "i am sure it is pretty far",
+      D: "Mercury",
+      correctAnswer: "C"
+   },
+    {
+      id: "saturnQuestionssssssss",
+      title: "How many rings does Saturn have?",
+      A: "it lives next door",
+      B: "no idea",
+      C: "i am sure it is pretty far",
+      D: "Mercury",
+      correctAnswer: "C"
+   },
+    {
+      id: "saturnQuestionssssssssssss",
+      title: "How many rings does Saturn have?",
+      A: "it lives next door",
+      B: "no idea",
+      C: "i am sure it is pretty far",
+      D: "Mercury",
+      correctAnswer: "C"
+   },
 ] ;
 
-// get the element with the id of 'quiznos'
-var quizContainer = document.getElementById('quiznos');
 
-// this loop is used to loop through the questions and create HTML elements to display the quiz
-questions.forEach(function(question) {
-   var div = document.createElement('div');
-   div.innerHTML = `
-   <h2>${question.title}</h2>
-   <label for="${question.id}" class="form_elem">
-   <input type="radio" name="${question.id}"  value="A">
-   ${question.A}
-    </label>
-    </label>
-       <label for="${question.id}" class="form_elem" >
-       <input type="radio" name="${question.id}"  value="B">
-       ${question.B}
-    </label>
-       <label for="${question.id}" class="form_elem">
-       <input type="radio" name="${question.id}"  value="C">
-       ${question.C}
-    </label>
-       <label for="${question.id}" class="form_elem">
-       <input type="radio" name="${question.id}"  value="D">
-       ${question.D}
-    </label>
-
-
-   `;
-   quizContainer.appendChild(div);
-});
-
-
+// get the element container for the quiz questions
+var quizContainer = document.getElementById('questions');
 // turn on variables to keep track of quiz points, answers and maximum amount of points.
 let rightAnswers = 0;
 var maxRightAnswers = questions.length;
@@ -76,6 +83,35 @@ let totalPoints = 0;
 var maxPoints = questions.length + "00";
 
 var selectedAnswer = "";
+
+// this loop is used to loop through the questions and create HTML elements to display the quiz.
+questions.forEach(function(question, index) {
+    var div = document.createElement('div');
+    div.id = `question-${index}`;
+    div.classList.add('question_cont');
+    div.innerHTML = `
+    <h2>${question.title}</h2>
+    <label for="${question.id}" class="form_elem">
+    <input type="radio" name="${question.id}"  value="A">
+    ${question.A}
+    </label>
+    </label>
+    <label for="${question.id}" class="form_elem" >
+    <input type="radio" name="${question.id}"  value="B">
+    ${question.B}
+    </label>
+    <label for="${question.id}" class="form_elem">
+    <input type="radio" name="${question.id}"  value="C">
+    ${question.C}
+    </label>
+    <label for="${question.id}" class="form_elem">
+    <input type="radio" name="${question.id}"  value="D">
+    ${question.D}
+    </label>
+    `;
+    quizContainer.appendChild(div);
+});
+
 
 // update the elements with the quiz answers.
 document.getElementById('total_questions').textContent = totalQuestions;
@@ -92,18 +128,40 @@ document.getElementById('quiznos').addEventListener('submit', function(e) {
 
 // A function that checks the correct answers with the users input.
 function checkAnswer(e) { 
-    questions.forEach(function(question) {
-        var selectedAnswer = document.querySelectorAll(`input[name="${question.id}"]:checked`);
+    questions.forEach(function(question, index) {
 
-// Check to see if user has submitted correct answers
+      var questionDiv = document.getElementById(`question-${index}`);
+      var selectedAnswer = questionDiv.querySelector(`input[name="${question.id}"]:checked`);
+      
+
+// Check to see if user has submitted correct answers.
         if (selectedAnswer && selectedAnswer.value === question.correctAnswer) { 
-            if (rightAnswers < maxRightAnswers) {
-                rightAnswers++;
+
+            if(!question.answeredCorrectly) {
+
+                if (rightAnswers < maxRightAnswers) {
+                    rightAnswers+= 1;
+                }
+                if (totalPoints < maxPoints) {
+                    totalPoints+= 100;
+                }
+                question.answeredCorrectly = true;
+                selectedAnswer.classlist.add('correct')
             }
-            if (totalPoints < maxPoints) {
-                totalPoints += 100;
+            if (questions.length === rightAnswers) {
+                console.log('you have finished the quiz!');
+               
+                alert('We win!!');
+             
+            document.querySelector('.checkAnswers').classlist.add('winner');
+            // selectedAnswered.classlist.add('correct');
             }
-            console.log('right answer: ' + question.correctAnswer);
+
+        console.log('right answer: ' + question.correctAnswer);
+        document.getElementById("total_questions").textContent = totalQuestions;
+        document.getElementById("right_answers").textContent = rightAnswers;
+        document.getElementById("total_points").textContent = totalPoints;
+        
         } else {
             console.log('wrong answer, correct was: ' + question.correctAnswer +  "" + question.title);
         }
@@ -114,7 +172,8 @@ function checkAnswer(e) {
 document.getElementById('quiznos').addEventListener('submit', function(e) {
     e.preventDefault();
     checkAnswer(e);
-
+ 
 });
+
 
 }
