7a0866cc74aa74073386523e71a7847265b97596
HollieMorrison <104519409+HollieMorrison@users.noreply.github.com>
2024-03-26 18:28:00 +0000

    Update: Javascript comments added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-
-
+// check if an element with the id 'quiznos' exists within the document
 if (document.getElementById('quiznos')) {
 
+// get the element with the id 'quiznos'
 var quiz = document.getElementById('quiznos');
 
-
+// array of questions, each question is an object that contains the question id, a title and options of the answers and shows the correct answer
 var questions = [
    {
       id: "moonQuestion",
@@ -35,9 +35,10 @@ var questions = [
    },
 ] ;
 
-
+// get the element with the id of 'quiznos'
 var quizContainer = document.getElementById('quiznos');
 
+// this loop is used to loop through the questions and create HTML elements to display the quiz
 questions.forEach(function(question) {
    var div = document.createElement('div');
    div.innerHTML = `
@@ -66,7 +67,7 @@ questions.forEach(function(question) {
 });
 
 
-  
+// turn on variables to keep track of quiz points, answers and maximum amount of points.
 let rightAnswers = 0;
 var maxRightAnswers = questions.length;
 let totalQuestions = questions.length;
@@ -76,11 +77,12 @@ var maxPoints = questions.length + "00";
 
 var selectedAnswer = "";
 
-
+// update the elements with the quiz answers.
 document.getElementById('total_questions').textContent = totalQuestions;
 document.getElementById('right_answers').textContent = rightAnswers;
 document.getElementById('total_points').textContent = totalPoints;
 
+// Event listener for submitting the answers.
 document.getElementById('quiznos').addEventListener('submit', function(e) {
     e.preventDefault();
     document.getElementById('right_answers').textContent = rightAnswers;
@@ -88,11 +90,12 @@ document.getElementById('quiznos').addEventListener('submit', function(e) {
 });
 
 
-
+// A function that checks the correct answers with the users input.
 function checkAnswer(e) { 
     questions.forEach(function(question) {
         var selectedAnswer = document.querySelectorAll(`input[name="${question.id}"]:checked`);
 
+// Check to see if user has submitted correct answers
         if (selectedAnswer && selectedAnswer.value === question.correctAnswer) { 
             if (rightAnswers < maxRightAnswers) {
                 rightAnswers++;
@@ -107,8 +110,7 @@ function checkAnswer(e) {
     });
 }
 
-
-
+// Event listener to check the answers when the answers are submitted.
 document.getElementById('quiznos').addEventListener('submit', function(e) {
     e.preventDefault();
     checkAnswer(e);
